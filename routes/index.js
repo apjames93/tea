aa04b3499b50ff1e0ac31508999fc35216e4fe5f
AmandaMontana Pearce <amandamontanapearce@gmail.com>
2016-06-15 17:10:35 -0600

    works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('tea_table').select().then(function (tea){
+    res.render('index', {list: tea, title: 'Look at the Tea'});
+})
 });
+
+router.get('/create', function(req, res, next){
+  res.render('create',{title: 'Add your favorite tea here!'});
+});
+
+router.get('/:id/edit', function(req, res,next){
+  knex('tea_table').where({id: req.params.id}).first().then(function (tea){
+    res.render('edit', {tea: tea})
+  })
+})
+
+router.post('/:id/edit', function(req, res,next){
+ knex('tea_table').where({id: req.params.id}).update(req.body).then(function (){
+   res.redirect('/' + req.params.id)
+ })
+})
+
+router.get('/:id/delete', function(req, res, next){
+  knex('tea_table').where({id: req.params.id}).del().then(function(){
+    res.redirect('/');
+  })
+})
+
+router.get('/:id', function(req, res, next){
+  knex('tea_table').where({id: req.params.id}).first().then(function(tea){
+    res.render('detail', {tea: tea})
+  })
+})
+
+router.post('/create', function(req, res, next){
+  knex('tea_table').insert(req.body).then(function(){
+    res.redirect('/');
+  }).catch( function(error){
+    console.log(error);
+  })
+})
 
 module.exports = router;
--- seeds/tea_seed.js
+++ seeds/tea_seed.js
@@ -5,20 +5,17 @@ exports.seed = function(knex, Promise) {
 
                         // Inserts seed entries
                         knex('tea_table').insert({
-                            id: 1,
                             name: 'earlgrey',
                             type: 'black',
                             caffine: true
                         }),
 
                         knex('tea_table').insert({
-                            id: 2,
                             name: 'breakfast',
                             type: 'black',
                             caffine: true
                         }),
                         knex('tea_table').insert({
-                            id: 3,
                             name: 'macha',
                             type: 'green',
                             caffine: true
